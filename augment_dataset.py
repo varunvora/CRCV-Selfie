ece09b7271f4900f1ef674a53a869958bc8e8c97
Varun Vora <varunvora@live.com>
2018-12-21 08:29:07 +0530

    corrected file name

--- augment_dataset.py
+++ augment_dataset.py
@@ -1,3 +1,5 @@
+from common import *
+
 from random import randrange
 from random import choice, random, sample
 from keras.preprocessing.image import ImageDataGenerator
@@ -12,47 +14,6 @@ from sklearn.model_selection import train_test_split
 
 from keras.applications.resnet50 import preprocess_input, decode_predictions, ResNet50
 
-# DATASET COLUMNS
-col = {
-    'imageName': 0,
-    'popularityScore': 1,
-    'partialFaces': 2,
-    'female': 3,
-    'baby': 4,
-    'child': 5,
-    'teenager': 6,
-    'youth': 7,
-    'middleAge': 8,
-    'senior': 9,
-    'white': 10,
-    'black': 11,
-    'asian': 12,
-    'ovalFace': 13,
-    'roundFace': 14,
-    'heartFace': 15,
-    'smiling': 16,
-    'mouthOpen': 17,
-    'frowning': 18,
-    'wearingGlasses': 19,
-    'wearingSunglasses': 20,
-    'wearingLipstick': 21,
-    'tongueOut': 22,
-    'duckFace': 23,
-    'blackHair': 24,
-    'blondHair': 25,
-    'brownHair': 26,
-    'redHair': 27,
-    'curlyHair': 28,
-    'straightHair': 29,
-    'braidHair': 30,
-    'showingCellphone': 31,
-    'usingEarphone': 32,
-    'usingMirror': 33,
-    'braces': 34,
-    'wearingHat': 35,
-    'harshLighting': 36,
-    'dimLighting': 37
-}
 
 datagen = ImageDataGenerator(
     featurewise_center=True,
@@ -110,5 +71,5 @@ if __name__ == "__main__":
         print(len(aug_df))
 
     aug_df = pd.DataFrame(aug_df)
-    aug_df.to_csv('./Augmented-Selfie-dataset/selfie-dataset.txt', sep=' ', index=False, header=None)
+    aug_df.to_csv('./Augmented-Selfie-dataset/selfie_dataset.txt', sep=' ', index=False, header=None)
 
